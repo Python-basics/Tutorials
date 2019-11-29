fb272468e811017a438e32a95b018c7eceef3bbf
Facundo Peillat <fpeillat@arvoia.com>
2019-11-29 19:03:02 +0000

    fix: free spaces

--- Tkinter_project.py
+++ Tkinter_project.py
@@ -13,13 +13,13 @@ win.geometry('300x300')
 
 def press(event):
     print('you pressed the button')
-    
+
 def exit_win():
     ans = mbox.askyesno('Exit','Are you sure?')
     if ans:
         win.destroy()
-        
-button = ttk.Button(win,text='Press')       
+
+button = ttk.Button(win,text='Press')
 button.bind('<Button-1>',press)
 button.bind('<Return>',press)   #  change
 button.bind('<Control_L>e',press)  # change
