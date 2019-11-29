b1ff1bd51c367a5e7a73555a8ea74feae8a892e9
Sam <30647159+Python-basics@users.noreply.github.com>
2019-11-28 18:28:34 -0600

    Create Tkinter_project.py

---
+++ Tkinter_project.py
@@ -0,0 +1,37 @@
+
+import tkinter as tk
+import tkinter.ttk as ttk
+import tkinter.messagebox as mbox
+
+win = tk.Tk()
+
+win.title("Window")
+
+#win.configure()
+
+win.geometry('300x300')
+
+def press(event):
+    print('you pressed the button')
+    
+def exit_win():
+    ans = mbox.askyesno('Exit','Are you sure?')
+    if ans:
+        win.destroy()
+        
+button = ttk.Button(win,text='Press')       
+button.bind('<Button-1>',press)
+button.bind('<Return>',press)   #  change
+button.bind('<Control_L>e',press)  # change
+
+
+button_exit = ttk.Button(win,text='Exit',command=exit_win)
+
+button.pack()
+button_exit.pack()
+#
+label = ttk.Label(win,text="Hello World")
+
+label.pack()
+
+win.mainloop()
